a59f95bb399d62a9637756f2ba7ece8fd504f595
Fabio Mangiameli <92296151+MangiameliFabio@users.noreply.github.com>
2022-12-16 15:04:40 +0100

    WIP

--- randomMapGenerator.cpp
+++ randomMapGenerator.cpp
@@ -1,33 +1,94 @@
 #include "randomMapGenerator.h"
-
 #include "core/math/random_number_generator.h"
-#include "modules/gridmap/grid_map.h"
-#include "scene/gui/grid_container.h"
 
 RandomMapGenerator::RandomMapGenerator() {
 }
 
-Array RandomMapGenerator::get_generated_map(const int width, const int height, int start, int end) {
+Array RandomMapGenerator::get_generated_map(const int width, const int height) {
 	Array generated_map;
 	print_line("map array created");
-	for (int x = 0; x < height; ++x) {
+	//Create Base Array
+	// std::vector<std::vector<int>> base_field(height, std::vector<int>(width, 0));
+	//
+	// for (int y = 0; y < height; ++y) {
+	// 	for (int x = 0; x < width; ++x) {
+	// 		const int gn = generate_number();
+	// 		for (int rule_col = 0; rule_col < map_objects_list_[gn].rule.size(); ++rule_col) {
+	//
+	// 		}
+	// 	}
+	// }
+	for (int y = 0; y < height; ++y) {
 		Array col;
-		for (int y = 0; y < width; ++y) {
-			col.append(generate_number(start, end));
+		for (int x = 0; x < width; ++x) {
+			col.append(0);
 		}
 		generated_map.append(col);
+	}
+
+	// Apply randomizer
+	for (int y = 0; y < height; ++y) {
+		for (int x = 0; x < width; ++x) {
+			if (static_cast<int>(static_cast<Array>(generated_map[y])[x]) == 0) {
+				const int gn = generate_number();
+				if (map_objects_list_[gn].has_rule) {
+					for (int rule_col = 0; rule_col < map_objects_list_[gn].rule.size(); ++rule_col) {
+						for (int index = 0; index < static_cast<Array>(map_objects_list_[gn].rule[rule_col]).size(); ++index) {
+							if (static_cast<int>(static_cast<Array>(map_objects_list_[gn].rule[rule_col])[index]) == 1) {
+								if (y + rule_col >= height || x + index >= width) {
+									continue;
+								}
+								if (static_cast<int>(static_cast<Array>(generated_map[y + rule_col])[x + index]) == 0) {
+									static_cast<Array>(generated_map[y + rule_col])[x + index] = gn;
+								}
+							}
+						}
+					}
+				}
+
+			}
+
+		}
 	}
 	print_line("finish");
 	return generated_map;
 }
 
-int RandomMapGenerator::generate_number(const int start, const int end) {
+int RandomMapGenerator::generate_number() {
+	if (map_objects_list_.empty()) {
+		print_line("No Objects registered for map generation");
+		return 0;
+	}
+	float weight_sum = 0;
+	for (const auto &object : map_objects_list_) {
+		weight_sum += object.weight;
+	}
 	RandomNumberGenerator rng;
 	rng.randomize();
-	return rng.randi_range(start, end);
+	float rnd = rng.randf_range(0, weight_sum);
+
+	for (int i = 0; i < static_cast<int>(map_objects_list_.size()); ++i) {
+		if (rnd < map_objects_list_[i].weight)
+			return i;
+		rnd -= map_objects_list_[i].weight;
+	}
+	print_line("No Object could be chosen");
+	return 0;
+}
+
+void RandomMapGenerator::add_new_object(const float weight, const Array &rule) {
+	MapObject object;
+	object.weight = weight;
+	object.index = static_cast<int>(map_objects_list_.size()) + 1;
+	if (!rule.empty()) {
+		object.has_rule = true;
+		object.rule = rule;
+	}
+	map_objects_list_.push_back(object);
 }
 
 //Bind all your methods used in this class
 void RandomMapGenerator::_bind_methods() {
-	ClassDB::bind_method(D_METHOD("get_generated_map", "width", "height", "start", "end"), &RandomMapGenerator::get_generated_map);
+	ClassDB::bind_method(D_METHOD("get_generated_map", "width", "height"), &RandomMapGenerator::get_generated_map);
+	ClassDB::bind_method(D_METHOD("add_new_object", "weight", "rule"), &RandomMapGenerator::add_new_object,DEFVAL(1.f),DEFVAL(Array()));
 }
--- randomMapGenerator.h
+++ randomMapGenerator.h
@@ -3,10 +3,19 @@
 #define RANDOMMAPGENERATOR_H
 
 #include "core/reference.h"
+#include "thirdparty/embree/kernels/common/accel.h"
+
+struct MapObject {
+	Array rule;
+	int index = 0;
+	float weight = 0;
+	boolean has_rule = false;
+};
 
 class RandomMapGenerator : public Reference {
 	GDCLASS(RandomMapGenerator, Reference);
 
+	std::vector<MapObject> map_objects_list_;
 
 protected:
 	static void _bind_methods();
@@ -14,10 +23,11 @@ protected:
 public:
 	RandomMapGenerator();
 
-	Array get_generated_map(int width, int height, int start, int end);
+	Array get_generated_map(int width, int height);
 
-	int generate_number(int start, int end);
+	int generate_number();
 
+	void add_new_object(float weight, const Array &rule);
 };
 
 #endif
